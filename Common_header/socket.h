c5823f92b101592f932da63790819da6f3a9af83
ThePiyushGupta <piyushg1411@gmail.com>
2020-03-13 16:35:38 +0530

    idk

--- Common_header/socket.h
+++ Common_header/socket.h
@@ -1,28 +1,28 @@
 #ifndef __socket_h
 #define __socket_h
 
-#include <dirent.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <sys/time.h>
-#include <time.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <netdb.h>
+#include <netinet/in.h>
 #include <signal.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <time.h>
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <sys/stat.h>
 #include <sys/uio.h>
-#include <unistd.h>
-#include <sys/wait.h>
 #include <sys/un.h>
-#include <strings.h>
-#include <stdbool.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #ifdef HAVE_SYS_IOCTL_H
 #include <sys/ioctl.h>
@@ -39,8 +39,8 @@
 #ifdef __osf__
 #undef recv
 #undef send
-#define recv(a,b,c,d) recvfrom(a,b,c,d,0,0)
-#define send(a,b,c,d) sendto(a,b,c,d,0,0)
+#define recv(a, b, c, d) recvfrom(a, b, c, d, 0, 0)
+#define send(a, b, c, d) sendto(a, b, c, d, 0, 0)
 #endif
 
 #ifndef INADDR_NONE
@@ -58,17 +58,17 @@
 #endif
 
 #ifndef HAVE_BZERO
-#define bzero(ptr,n) memset(ptr, 0, n)
+#define bzero(ptr, n) memset(ptr, 0, n)
 #define HAVE_BZERO
 #endif
 
 #ifndef HAVE_GETHOSTBYNAME2
-#define gethostbyname2(host,family) gethostbyname((host))
+#define gethostbyname2(host, family) gethostbyname((host))
 #endif
 
 struct inpktinfo {
-  struct in_addr ipi_addr;
-  int ipi_ifindex;
+    struct in_addr ipi_addr;
+    int ipi_ifindex;
 };
 
 #ifndef CMSG_LEN
@@ -80,7 +80,8 @@ struct inpktinfo {
 #endif
 
 #ifndef SUN_LEN
-#define SUN_LEN(su) (sizeof(*(su)) - sizeof((su)->sun_path) + strlen((su)->sun_path))
+#define SUN_LEN(su) \
+    (sizeof(*(su)) - sizeof((su)->sun_path) + strlen((su)->sun_path))
 #endif
 
 #ifndef AF_LOCAL
@@ -90,7 +91,8 @@ struct inpktinfo {
 #define PF_LOCAL PF_UNIX
 #endif
 
-#define LISTENQ 1024
+#define LISTENQ 0
+#define ACCEPTQ 16
 #define MAXLINE 4096
 #define MAXSOCKADDR 128
 #define BUFFSIZE 8192
@@ -104,15 +106,15 @@ struct inpktinfo {
 #define FILE_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
 #define DIR_MODE (FILE_MODE | S_IXUSR | S_IXGRP | S_IXOTH)
 
-typedef void Sigfunc (int);
+typedef void Sigfunc(int);
 
-#define min(a,b) ((a) < (b) ? (a) : (b))
-#define max(a,b) ((a) > (b) ? (a) : (b))
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
 
 #ifndef HAVE_IF_NAMEINDEX_STRUCT
 struct if_nameindex {
-  unsigned int if_index;
-  char *if_name;
+    unsigned int if_index;
+    char *if_name;
 };
 #endif
 #endif
